88c6b35ddb1518ee9a5449819a6fe50966c30a46
Shashank P <shashankp2832@gmail.com>
2022-08-07 11:01:59 +0530

    Added comments in Clock and Main file

--- Assignment0/Scripts/Clock.java
+++ Assignment0/Scripts/Clock.java
@@ -1,7 +1,9 @@
 public class Clock {
 
+    // Declaring time attribute, starting from 0
     static int time = 0;
 
+    // Method to increment time
     static public void increment(int delta) {
         time += delta;
     }
--- Assignment0/Scripts/Main.java
+++ Assignment0/Scripts/Main.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     
+    // Check whether the intruder can move forward
     static boolean can_move_forward(boolean[][] border)
     {
         return !(border[0][0] && border[0][1] && border[0][2]) && !(border[1][1]);
@@ -9,6 +10,7 @@ public class Main {
 
     public static void main(String args[])
     {
+        // Creating objects of different classes
         int w = Integer.parseInt(args[0]);
         double p = Double.parseDouble(args[1]);
         Clock clock = new Clock();
@@ -16,6 +18,7 @@ public class Main {
         Intruder intruder = new Intruder(w);
         Border border = new Border(p, w);
 
+        // Interate until the intruder reaches the defending country
         while(!intruder.goal_test())
         {
             boolean[][] current_sensor_config = border.get_env(intruder.position);
