7410a15d35b16f31dae6366cf17906a372ff5f0c
Shashank P <shashankp2832@gmail.com>
2022-08-17 17:21:53 +0530

    Fixed Even-Odd, added Prime number

--- Assignment1/supporting_files/program_templates/prime_template.asm
+++ Assignment1/supporting_files/program_templates/prime_template.asm
@@ -1,8 +1,24 @@
 	.data
 a:
-	10
+	15485863
 	.text
-// your code here to check if the number 'a' is prime. If yes, write 1 to x10. Else write -1.
-// you may change the value of 'a'
-// remove these comments!
-
+main:
+	load %x0, $a, %x3
+	addi %x0, 1, %x6
+	addi %x0, 2, %x7
+	beq %x3, %x6, nprime
+	beq %x3, %x7, prime
+	addi %x0, 2, %x4
+	jmp iter
+iter:
+	div %x3, %x4, %x5
+	beq %x31, 0, nprime
+	addi %x4, 1, %x4
+	blt %x4, %x3, iter
+	beq %x4, %x3, prime
+prime:
+	addi %x0, 1, %x10
+	end
+nprime:
+	subi %x0, 1, %x10
+	end
