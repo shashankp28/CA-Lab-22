bc2c1d21c903971eb882e90d71c2812095080185
harrithha <harrithha64@gmail.com>
2022-09-05 19:22:24 +0530

    Parts

--- Assignment2/cs311_assignment2/src/generic/Simulator.java
+++ Assignment2/cs311_assignment2/src/generic/Simulator.java
@@ -1,5 +1,6 @@
 package generic;
 
+import java.nio.ByteBuffer;
 import java.io.*;
 import generic.Operand.OperandType;
 
@@ -19,16 +20,31 @@ public class Simulator {
 	{
 		//TODO your assembler code
 		//1. open the objectProgramFile in binary mode
-		FileOutputStream fos = new FileOutputStream(new File(objectProgramFile));
-		fos.write("Hey, there!".getBytes());
-		fos.write("\n".getBytes());
-		fos.write("How are you doing?".getBytes());
-		fos.close();
-		//2. write the firstCodeAddress to the  file
+		FileOutputStream binary_file = new FileOutputStream(objectProgramFile);
+		BufferedOutputStream out_file = new BufferedOutputStream(binary_file);
+		// fos.write("Hey, there!".getBytes());
+		// fos.write("\n".getBytes());
+		// fos.write("How are you doing?".getBytes());
 		
+		//2. write the firstCodeAddress to the  file
+		byte[] fca = ByteBuffer.allocate(4).putInt(ParsedProgram.firstCodeAddress).array();
+		out_file.write(fca);
+
 		//3. write the data to the file
+		for(var data: ParsedProgram.data)
+		{	
+			byte[] read = ByteBuffer.allocate(4).putInt(data).array();
+			out_file.write(read);
+
+		}
+
 		//4. assemble one instruction at a time, and write to the file
 		//5. close the file
+		out_file.close();
 	}
 	
+	public static void main(String[] args){
+
+
+	}
 }
