a06f3ae0c15f8d29eb9609eb11073176865e1efb
Shashank P <shashankp2832@gmail.com>
2022-10-17 18:43:24 +0530

    My commit a5

--- Assignment5/src/processor/pipeline/MemoryAccess.java
+++ Assignment5/src/processor/pipeline/MemoryAccess.java
@@ -19,41 +19,42 @@ public class MemoryAccess {
 	
 	public void performMA()
 	{
-		//TODO
-		if (EX_MA_Latch.getNop()) {
-			MA_RW_Latch.setNop(true);
-			System.out.println("MA - MA_RW_Nop: True");
-			MA_RW_Latch.setInstruction(null);
-			System.out.println("MA - MA_RW_Inst: Null");
-			EX_MA_Latch.setNop(false);
-			System.out.println("MA - EX_MA_Nop: False");
-		} else if (EX_MA_Latch.isMA_enable())
-		{
-	
-			Instruction inst = EX_MA_Latch.getInstruction();
-			OperationType ot = inst.getOperationType();
-			int alu_result = EX_MA_Latch.getALU_result();
-			MA_RW_Latch.setALU_result(alu_result);
-
-			if (ot == OperationType.load) {
-
-				int lr = containingProcessor.getMainMemory().getWord(alu_result);
-				MA_RW_Latch.setLoad_result(lr);
-			}
-			else if (ot == OperationType.store) {
-
-				int str = containingProcessor.getRegisterFile().getValue(inst.getSourceOperand1().getValue());
-				containingProcessor.getMainMemory().setWord(alu_result, str);
-			}
-			if (inst.getOperationType().ordinal() == 29) 
+		if(!EX_MA_Latch.getBusy()){
+			if (EX_MA_Latch.getNop()) {
+				MA_RW_Latch.setNop(true);
+				System.out.println("MA - MA_RW_Nop: True");
+				MA_RW_Latch.setInstruction(null);
+				System.out.println("MA - MA_RW_Inst: Null");
+				EX_MA_Latch.setNop(false);
+				System.out.println("MA - EX_MA_Nop: False");
+			} else if (EX_MA_Latch.isMA_enable())
 			{
-				IF_EnableLatch.setIF_enable(false);
-				System.out.println("MA - IF_Enable: False (end)");
-			} 
-	
-			MA_RW_Latch.setInstruction(inst);
-			MA_RW_Latch.setRW_enable(true);
-			System.out.println("MA - RW_Enable: True");
+		
+				Instruction inst = EX_MA_Latch.getInstruction();
+				OperationType ot = inst.getOperationType();
+				int alu_result = EX_MA_Latch.getALU_result();
+				MA_RW_Latch.setALU_result(alu_result);
+
+				if (ot == OperationType.load) {
+
+					int lr = containingProcessor.getMainMemory().getWord(alu_result);
+					MA_RW_Latch.setLoad_result(lr);
+				}
+				else if (ot == OperationType.store) {
+
+					int str = containingProcessor.getRegisterFile().getValue(inst.getSourceOperand1().getValue());
+					containingProcessor.getMainMemory().setWord(alu_result, str);
+				}
+				if (inst.getOperationType().ordinal() == 29) 
+				{
+					IF_EnableLatch.setIF_enable(false);
+					System.out.println("MA - IF_Enable: False (end)");
+				} 
+		
+				MA_RW_Latch.setInstruction(inst);
+				MA_RW_Latch.setRW_enable(true);
+				System.out.println("MA - RW_Enable: True");
+			}
 		}
 	}
 
